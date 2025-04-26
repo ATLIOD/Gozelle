28238e5142f0a370f316433beeb185adbaa6ccb1
ATLIOD <60109469+ATLIOD@users.noreply.github.com>
2025-04-25 23:20:21 -0500

    quick sort function to use for sorting datastore

--- internal/db/store.go
+++ internal/db/store.go
@@ -243,3 +243,25 @@ func (dm *DirectoryManager) DetermineFilthy() error {
 func (dm *DirectoryManager) SwapRemove(dir *Directory) error {
 	return nil
 }
+
+func quickSort(arr []*Directory, low, high int) {
+	if low < high {
+		pi := partition(arr, low, high)
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+	}
+}
+
+func partition(arr []*Directory, low, high int) int {
+	pivot := arr[high]
+	i := low - 1
+
+	for j := low; j < high; j++ {
+		if arr[j].Path < pivot.Path {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i + 1
+}
