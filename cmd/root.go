ded6d22a25e9b07cfc218fa05b533ab7625e92fb
ATLIOD <60109469+ATLIOD@users.noreply.github.com>
2025-05-20 21:00:00 -0500

    changed main to call rootcmd and added root and registrations of other commadn in cmd/root.go

---
+++ cmd/root.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "gozelle",
+	Short: "Gozelle - smart directory jumper",
+}
+
+func init() {
+	RootCmd.AddCommand(InitCmd)
+	RootCmd.AddCommand(QueryCmd)
+	RootCmd.AddCommand(AddCmd)
+	RootCmd.AddCommand(RemoveCmd)
+	RootCmd.AddCommand(ListCmd)
+	RootCmd.AddCommand(InteractiveCmd)
+	RootCmd.AddCommand(HelpCmd)
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/atliod/gozelle/cmd"
@@ -12,60 +11,8 @@ import (
 func main() {
 	core.SetConfig()
 
-	if len(os.Args) < 2 {
-		cmd.HelpCmd.Run(cmd.HelpCmd, []string{})
-		os.Exit(1)
-	}
-
-	keywords := os.Args[2:]
-
-	switch os.Args[1] {
-	case "init":
-		cmd.InitCmd.Run(cmd.InitCmd, keywords)
-	case "query":
-		// Call the query function
-		result := cmd.QueryTop(keywords, os.Getenv("GOZELLE_DATA_DIR"))
-		if result.Path == nil {
-			log.Println("No match found")
-			return
-		}
-		if os.Getenv("GOZELLE_ECHO") == "true" {
-			log.Println("jumped to:", result.Path.Path)
-		}
-		core.Prune()
-		// log.Println("Frecency:", result.Frecency)
-		// log.Println("Last visit:", result.Path.LastVisit)
-		// log.Println("Score:", result.Path.Score)
-	case "add":
-		// Call the add function
-		cmd.Add(os.Args[2])
-		core.Prune()
-
-	case "remove":
-		// Call the remove function
-		cmd.Remove(os.Args[2])
-		core.Prune()
-	case "list":
-		// Call the list function
-		cmd.List()
-		core.Prune()
-	case "help":
-		// Call the help function
-		cmd.HelpCmd.Run(cmd.HelpCmd, keywords)
-	case "interactive":
-		core.CheckFzfInstalled()
-		result, err := cmd.QueryInteractive(os.Getenv("GOZELLE_DATA_DIR"), false)
-		if err != nil {
-			fmt.Println("Error:", err)
-		}
-		if os.Getenv("GOZELLE_ECHO") == "true" {
-			log.Println("jumped to:", result)
-		}
-
-		core.Prune()
-	default:
-		fmt.Printf("Unknown command: %s\n", os.Args[1])
-		cmd.HelpCmd.Run(cmd.HelpCmd, keywords)
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
